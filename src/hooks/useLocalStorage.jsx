646d6f7596360d44b31faaa408e1a08c17dbe37e
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-02-19 01:28:22 +0530

    added

---
+++ src/hooks/useLocalStorage.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+const PREFIX = 'codepen-clone-';
+
+export default function useLocalStorage(key, initialValue) {
+  const prefixedKey = PREFIX + key;
+
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(prefixedKey);
+    if (jsonValue != null) return JSON.parse(jsonValue);
+
+    if (typeof initialValue === 'function') {
+      return initialValue();
+    } else {
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(prefixedKey, JSON.stringify(value));
+  }, [prefixedKey, value]);
+
+  const setValueWithLocalStorage = (newValue) => {
+    const newValueObject = typeof newValue === 'function' ? newValue(value) : newValue;
+    setValue(newValueObject);
+    localStorage.setItem(prefixedKey, JSON.stringify(newValueObject));
+  };
+
+  return [value, setValueWithLocalStorage];
+}
