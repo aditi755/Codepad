38d3d9eccb32e9209595255d347d16dee7bfd83c
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-02-19 13:55:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './src/main.jsx', // Specify the entry JavaScript file
-      },
-    },
-        // Specify additional assets to be copied to the dist directory
-        assetsDir: '.', // This will include all files in the root directory
-
-  },
-});
-
+})
