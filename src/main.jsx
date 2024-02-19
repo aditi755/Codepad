ce07cfe1d3b209c5503b1cfbdf8560ff2d239867
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-02-19 13:50:19 +0530

    e

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from "react-dom";
+// import App from './App.jsx'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App/App.tsx";
+// import "./index.css";
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
